ffc9a592d1963986a84472626d5538574c8479b3
AshwinGoutam77 <ashwin@devwings.com>
2025-12-18 14:40:54 +0530

    latest code

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,12 @@
-import path from 'path';
-
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
+      ssl: env.bool('DATABASE_SSL', true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-    pool: { min: 2, max: 10 },
-    acquireConnectionTimeout: 60000,
+    pool: { min: 0, max: 5 },
   },
 });
